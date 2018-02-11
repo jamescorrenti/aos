a62d098afb51143126d2072b911a194bfa37b0bc
James Correnti <jcorrenti3@gatech.edu>
2018-02-11 12:17:42 -0800

    building project1 structure

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 All the work I did for Advanced OS
 
 # Project 0
+
+
+## Other notes:
+
+###Useful Sublime packages - should figure out how to automate
+
+SublimeLinter and SublimeLinter-gcc 
+-----------------------------------
+Key Bindings
+Hotkey			Cmd
+CTRL + K, L 	Lint this view
+CTRL + K, A 	Show all errors
+CTRL + K, N 	Goto Next Error
+CTRL + K, P 	Goto Prev error
+
---
+++ project1/start_vm.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+vmname=test1
+release=xenial
+
+if [[ $(uvt-simplestreams-libvirt query) ]] ; then
+    echo "cloud already exists"
+else
+	echo "Syncing cloud image"
+    uvt-simplestreams-libvirt sync release=$release arch=amd64
+fi
+
+echo "checking if VM created"
+
+if [[ $(uvt-kvm list | grep $vmname) ]]; then
+    echo "$vmname already exists"
+else
+	echo "Creating VM $vmname"
+    uvt-kvm create $vmname release=$release
+    echo "Sleeping....."
+    sleep 30
+fi
+
+echo "Connecting to VM $vmname"
+uvt-kvm ssh $vmname --insecure
+

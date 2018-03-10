6a94c78953c01a88d5907f9b95167eb0c470b875
James Correnti <jcorrenti3@gatech.edu>
2018-03-10 16:00:56 -0500

    counter complete

--- project2/gtmp_counter.c
+++ project2/gtmp_counter.c
@@ -1,5 +1,8 @@
 #include <omp.h>
 #include "gtmp.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 /*
     From the MCS Paper: A sense-reversing centralized barrier
@@ -17,15 +20,45 @@
            repeat until sense = local_sense
 */
 
+bool sense = true;
+bool local_sense = false;
+static int total_threads;
+int count;
+
+
+int fetch_and_decrement(int *count){
+    int new_count;
+    #pragma omp atomic read 
+        new_count = *count;
+    
+    #pragma omp atomic write
+        *count = new_count - 1;
+    return new_count;
+}
 
 void gtmp_init(int num_threads){
-
+    total_threads = num_threads;
+    count = num_threads;
 }
 
 void gtmp_barrier(){
-
+    
+    if (fetch_and_decrement(&count) == 1)
+    {
+        printf("Unlocking\n");
+        count = total_threads;
+        sense = local_sense;
+    }
+    else
+    {
+        while(sense != local_sense)
+        {
+            printf("Thread spinning\n");
+            usleep(1);
+        }
+    }
+    printf("Thread Exiting\n");
 }
 
 void gtmp_finalize(){
-
 }
